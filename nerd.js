9773e9d6df3b4f60d8a9eb9dc832292a49588bdc
pasqLisena <pasq.lisena@gmail.com>
2014-04-04 11:33:57 +0200

    c

---
+++ nerd.js
@@ -0,0 +1,33 @@
+var nerd = require("nerd4node"),
+    filesys = require("fs");
+
+var api_instance = "nerd.eurecom.fr/api/";
+var apiID = '1qb6bi7kjmcudkh5gsqr79ufmflo4mlu';
+var ext = 'dbspotlight';
+var doc_type = "text"; //timedtext|text
+var t = "";
+var gran = "oed";
+var to = 2;
+var nerd_results;
+var callback = function (something) {
+    console.log('*****');
+    console.log(something);
+    nerd_results = something;
+}
+
+function start(callback) {
+    filesys.readFile("./text.txt", "binary", function (err, file) {
+        if (err) {
+            callback(err, "file unreadable");
+        } else {
+            t = file;
+            nerd.annotate(api_instance, apiID, ext, doc_type, t, gran, to, function (err, data) {
+                callback(err, data);
+            });
+
+
+        }
+    });
+}
+
+exports.start = start;
--- server.js
+++ server.js
@@ -2,33 +2,53 @@ var sys = require("sys"),
     my_http = require("http"),
     path = require("path"),
     url = require("url"),
-    filesys = require("fs");
+    filesys = require("fs"),
+    nerd = require('./nerd');
 
 my_http.createServer(function (request, response) {
     var my_path = url.parse(request.url).pathname;
-    var full_path = path.join(process.cwd(), my_path);
-    filesys.exists(full_path, function (exists) {
-        if (!exists) {
-            response.writeHeader(404, {"Content-Type": "text/plain"});
-            response.write("404 Not Found\n");
-            response.end();
-        }
-        else {
-            filesys.readFile(full_path, "binary", function (err, file) {
-                if (err) {
-                    response.writeHeader(500, {"Content-Type": "text/plain"});
-                    response.write(err + "\n");
-                    response.end();
 
-                }
-                else {
-                    response.writeHeader(200);
-                    response.write(file, "binary");
-                    response.end();
-                }
+    if (my_path === '/nerd') {
+        nerd.start(function (err, data) {
+            if (err){
+                sendResponse(500, "text/plain", data);
+            }else{
+                sendResponse(200, "text/plain", JSON.stringify(data));
+            }
+        });
+    } else {
 
-            });
+        var full_path = path.join(process.cwd(), my_path);
+        filesys.exists(full_path, function (exists) {
+            if (!exists) {
+                sendResponse(404, "text/plain", "404 Not Found\n");
+            }
+            else {
+                filesys.readFile(full_path, "binary", function (err, file) {
+                    if (err) {
+                        sendResponse(500, "text/plain", err + "\n");
+                    }
+                    else {
+                        sendResponse(200, "auto", file, "binary");
+                    }
+
+                });
+            }
+        });
+    }
+
+    function sendResponse(code, content_type, content, format) {
+        if (content_type === "auto") {
+            response.writeHeader(code);
+        } else {
+            response.writeHeader(code, {"Content-Type": content_type});
         }
-    });
+        if (format) {
+            response.write(content, format);
+        } else {
+            response.write(content);
+        }
+        response.end();
+    }
 }).listen(8080);
-sys.puts("Server Running on 8080");			
+sys.puts("Server Running on 8080");
