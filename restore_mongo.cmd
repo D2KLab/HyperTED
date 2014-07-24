31276ac4b528a51fd4cb476fed81fc7e04146508
Mariella Sabatino <mll.sabatino@gmail.com>
2014-07-24 16:36:50 +0200

    trying to revert

--- restore_mongo.cmd
+++
@@ -1,2 +0,0 @@
-cd C:\Program Files\MongoDB 2.6 Standard\bin
-mongorestore -d hyperted C:\Users\Mary\Documents\GitHub\MediaFragPlayerDemo\database\hyperted\
--- start_mongo.cmd
+++
@@ -1,2 +0,0 @@
-cd C:\Program Files\MongoDB 2.6 Standard\bin
-mongod --dbpath C:\Users\Mary\Documents\GitHub\MediaFragPlayerDemo\database
